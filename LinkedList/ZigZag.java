7510149c27033508883391d4ba09767ba3533915
em.email <heysunny.1212@gmail.com>
2025-09-20 19:02:40 +0530

    ZigZag LL

---
+++ LinkedList/ZigZag.java
@@ -0,0 +1,95 @@
+// package LinkedList;
+
+public class ZigZag {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    // 1. Find mid
+    private Node getMid(Node head) {
+        Node slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // 2. Reverse list
+    private Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    // 3. Rearrange in ZigZag fashion
+    public void zigZag(Node head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        Node mid = getMid(head);
+
+        // split the list
+        Node first = head;
+        Node second = reverse(mid.next);
+        mid.next = null;
+
+        while (first != null && second != null) {
+            Node temp1 = first.next;
+            Node temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+
+    }
+
+    // Print list
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ZigZag list = new ZigZag();
+        list.head = new Node(1);
+        list.head.next = new Node(2);
+        list.head.next.next = new Node(3);
+        list.head.next.next.next = new Node(4);
+        list.head.next.next.next.next = new Node(5);
+
+        System.out.println("Original List:");
+        list.printList();
+
+        list.zigZag(list.head);
+
+        System.out.println("ZigZag List:");
+        list.printList();
+    }
+
+}
