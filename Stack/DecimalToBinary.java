73271bd3159a2f97ed39d250b60bb56a842ca249
em.email <heysunny.1212@gmail.com>
2025-09-25 23:44:40 +0530

    print 1 - N decimal to binaey using stack

---
+++ Stack/DecimalToBinary.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class DecimalToBinary {
+
+  public static void printDecimalToBinary(int N) {
+    Stack<Integer> q = new Stack<>();
+
+    for (int i = 1; i <= N; i++) {
+      int temp = i;
+      while (temp != 0) {
+        int binDigit = temp % 2;
+        temp = temp / 2;
+        q.push(binDigit);
+      }
+
+      while (!q.isEmpty()) {
+        System.out.print(q.pop());
+      }
+      System.out.print(" ");
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    printDecimalToBinary(3);
+
+  }
+}
