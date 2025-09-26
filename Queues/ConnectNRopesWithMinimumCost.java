eec8fa35070098f189b0738420d2947b40d1dbd4
em.email <heysunny.1212@gmail.com>
2025-09-26 09:12:43 +0530

    Connect n ropes with minimum cost

---
+++ Queues/ConnectNRopesWithMinimumCost.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+
+public class ConnectNRopesWithMinimumCost {
+    public static int minCost(int[] ropes) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int cost = 0;
+
+        for (int rope : ropes) {
+            pq.add(rope);
+        }
+
+        while (pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+
+            int newRope = first + second;
+            pq.add(newRope);
+            cost += newRope;
+        }
+        return cost;
+    }
+
+    public static void main(String[] args) {
+        int ropes[] = { 1, 2, 3 };
+
+        System.out.println(minCost(ropes));
+
+    }
+}
